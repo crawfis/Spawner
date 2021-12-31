7bd41fc22e198fc768d13b005a0ad55ff6041688
Roger Crawfis <crawfis.3@osu.edu>
2021-12-31 14:02:42 -0500

    Added a class design on the functional ObjectPool to handle common cases easily.

--- Assets/CrawfisSoftware/Scripts/Spawner/Generators/PrefabGeneratorPooled.cs
+++ Assets/CrawfisSoftware/Scripts/Spawner/Generators/PrefabGeneratorPooled.cs
@@ -9,7 +9,7 @@ namespace CrawfisSoftware.Spawner
     /// </summary>
     public class PrefabGeneratorPooled : IPrefabGeneratorAsync
     {
-        private readonly ObjectPool<GameObject> prefabPool;
+        private readonly IPooler<GameObject> prefabPool;
         private readonly GameObject prefab;
 
         /// <summary>
@@ -18,8 +18,7 @@ namespace CrawfisSoftware.Spawner
         /// <param name="prefab">The game object to use as a template for new game objects.</param>
         public PrefabGeneratorPooled(GameObject prefab, int defaultCapacity = 100, int maxPersistentSize = 10000)
         {
-            prefabPool = new ObjectPool<GameObject>(CreateNewPooledPrefab, GetPooledPrefab, ReleasePooledPrefab, 
-                DestroyPooledPrefab, true, defaultCapacity, maxPersistentSize);
+            prefabPool = new GameObjectPooler(prefab);
             this.prefab = prefab;
         }
 
@@ -30,9 +29,5 @@ namespace CrawfisSoftware.Spawner
             newGameObject.transform.localPosition = position;
             return await Task.FromResult<GameObject>(newGameObject);
         }
-        private GameObject CreateNewPooledPrefab() => UnityEngine.Object.Instantiate<GameObject>(prefab);
-        private void GetPooledPrefab(GameObject prefab) => prefab.gameObject.SetActive(true);
-        private void ReleasePooledPrefab(GameObject prefab) => prefab.gameObject.SetActive(false);
-        private void DestroyPooledPrefab(GameObject prefab) => UnityEngine.Object.Destroy(prefab);
     }
 }
